88645d0242d3f04e8d171eb0682401221f2a3cea
Alvin-Karanja <alvin2@proton.me>
2023-08-14 23:57:02 +0300

    added form and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import "./styles.css"
 
 export default function App() {
-    return <form className="new-item-form">
-        <div className="form-row">
-            <label>New Item</label>
-        </div>
-    </form>
+    return (
+        <>
+            <form className="new-item-form">
+                <div className="form-row">
+                    <label htmlFor="item">New Item</label>
+                    <input type="text" id="item" />
+                </div>
+                <button className="btn">Add</button>
+            </form>
+            <h1 className="header">Todo List</h1>
+        </>
+    )
 }
